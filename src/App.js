4575d495c2947a0ad4a400c8499a4d5f482e3bf6
kumarrahul <kumar.rahul@vedantu.com>
2018-09-25 20:27:36 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-//import { Navbar, NavbarBrand } from 'reactstrap';
-//import { DISHES } from './shared/dishes';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     dishes: DISHES
-  //   };
-  // }
-    
+
   render() {
     return (
+    <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }

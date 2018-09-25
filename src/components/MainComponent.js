e13c664766eaddae2ac56693e3e80393f91a3bf0
kumarrahul <kumar.rahul@vedantu.com>
2018-09-26 01:44:10 +0530

    spa-2 | dishdetails routin | url param

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -38,12 +38,13 @@ class Main extends Component {
         );
       }      
 
-    //   const DishWithId = ({match}) => {
-    //     return (
-    //       <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} />
-    //     );
-    //   };
-  
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+    };
+
 
     return (
       <div>
@@ -51,7 +52,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              {/* <Route exact path="/menu/:dishId" component={DishWithId} /> */}
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
         </Switch>
